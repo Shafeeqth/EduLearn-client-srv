b1eb04e9d2757c24552932de2178cb7033128e1d
shafeeque <shafeeqsha06@gmail.com>
2025-10-01 22:29:26 +0530

    feat(auth): add auth layout

---
+++ src/app/(common)/auth/layout.tsx
@@ -0,0 +1,29 @@
+import React, { Suspense } from 'react';
+import { Metadata } from 'next';
+import Loading from './loading';
+// import { getServerSession } from 'next-auth';
+// import { authOptions } from '@/app/api/auth/[...nextauth]/_nextAuth';
+// import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Authentication ',
+  description: 'Sign in to your EduLearn account or create a new one.',
+};
+
+export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+  // const session = await getServerSession(authOptions);
+  // const cookies =
+  // console.log(JSON.stringify(session, null, 2));
+
+  // if (session?.user) {
+  //   // User is logged in, redirect to home or dashboard
+  //   redirect('/');
+  // }
+  return (
+    // <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900">
+    //   <div className="flex min-h-screen items-center justify-center p-4 sm:p-6 lg:p-8">
+    <Suspense fallback={<Loading />}>{children}</Suspense>
+    //   </div>
+    // </div>
+  );
+}
